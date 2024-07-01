8029bbd29789d8b414c6990b585f95496cb568c1
dmpierre <pdaixmoreux@gmail.com>
2024-07-01 18:02:19 +0200

    feat: external inputs can be empty

--- folding-schemes/src/frontend/noname/mod.rs
+++ folding-schemes/src/frontend/noname/mod.rs
@@ -1,12 +1,14 @@
-use std::marker::PhantomData;
-use std::str::FromStr;
-
 use crate::Error;
+use ark_noname::NoNameCircuit;
+use ark_r1cs_std::alloc::AllocVar;
 use ark_r1cs_std::fields::fp::FpVar;
 use ark_r1cs_std::R1CSVar;
+use ark_relations::r1cs::ConstraintSynthesizer;
 use noname::inputs::{parse_inputs, JsonInputs};
 use num_bigint::BigUint;
 use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
+use std::str::FromStr;
 
 use super::FCircuit;
 use ark_ff::PrimeField;
@@ -36,9 +38,10 @@ impl<F: PrimeField> From<Vec<F>> for NoNameJSONIVCInput {
 
 impl NoNameJSONIVCInput {
     fn from_fpvars<F: PrimeField>(value: &Vec<FpVar<F>>) -> Self {
-        let field_elements = Vec::<F>::with_capacity(value.len());
+        let mut field_elements = Vec::<F>::with_capacity(value.len());
         for var in value {
             let val = var.value().unwrap();
+            field_elements.push(val);
         }
         NoNameJSONIVCInput::from(field_elements)
     }
@@ -56,9 +59,10 @@ impl<F: PrimeField> From<Vec<F>> for NoNameJSONExternalInputs {
 
 impl NoNameJSONExternalInputs {
     fn from_fpvars<F: PrimeField>(value: &Vec<FpVar<F>>) -> Self {
-        let field_elements = Vec::<F>::with_capacity(value.len());
+        let mut field_elements = Vec::<F>::with_capacity(value.len());
         for var in value {
             let val = var.value().unwrap();
+            field_elements.push(val);
         }
         NoNameJSONExternalInputs::from(field_elements)
     }
@@ -66,35 +70,46 @@ impl NoNameJSONExternalInputs {
 
 pub struct NoNameIVCInputs<F: PrimeField> {
     pub ivc_input: Vec<F>,
-    pub external_inputs: Vec<F>,
+    pub external_inputs: Option<Vec<F>>,
 }
 
-impl<F: PrimeField> TryFrom<(&str, &str)> for NoNameIVCInputs<F> {
+impl<F: PrimeField> TryFrom<(String, Option<String>)> for NoNameIVCInputs<F> {
     type Error = Error;
 
-    fn try_from(value: (&str, &str)) -> Result<Self, Error> {
+    fn try_from(value: (String, Option<String>)) -> Result<Self, Error> {
         let (ivc_public_inputs, external_inputs) = value;
-        let json_ivc_inputs = serde_json::from_str::<NoNameJSONIVCInput>(ivc_public_inputs)
+        let json_ivc_inputs = serde_json::from_str::<NoNameJSONIVCInput>(&ivc_public_inputs)
             .map_err(|e| Error::Other(format!("{:?}", e)))?;
-        let json_external_inputs =
-            serde_json::from_str::<NoNameJSONExternalInputs>(external_inputs)
-                .map_err(|e| Error::Other(format!("{:?}", e)))?;
+
+        let json_external_inputs = match external_inputs {
+            Some(inputs) => Some(
+                serde_json::from_str::<NoNameJSONExternalInputs>(&inputs)
+                    .map_err(|e| Error::Other(format!("{:?}", e)))?,
+            ),
+            None => None,
+        };
 
         let mut ivc_input = Vec::with_capacity(json_ivc_inputs.ivc_input.len());
-        let mut external_inputs = Vec::with_capacity(json_external_inputs.external_inputs.len());
-
         for str_input in json_ivc_inputs.ivc_input.iter() {
             let parsed_biguint =
                 BigUint::from_str(&str_input).map_err(|e| Error::Other(format!("{:?}", e)))?;
             let field_element = F::from_le_bytes_mod_order(&parsed_biguint.to_bytes_be());
             ivc_input.push(field_element);
         }
-        for str_input in json_external_inputs.external_inputs.iter() {
-            let parsed_biguint =
-                BigUint::from_str(&str_input).map_err(|e| Error::Other(format!("{:?}", e)))?;
-            let field_element = F::from_le_bytes_mod_order(&parsed_biguint.to_bytes_be());
-            external_inputs.push(field_element);
-        }
+
+        let external_inputs = match json_external_inputs {
+            Some(inputs) => {
+                let mut external_inputs = Vec::with_capacity(inputs.external_inputs.len());
+                for str_input in inputs.external_inputs.iter() {
+                    let parsed_biguint = BigUint::from_str(&str_input)
+                        .map_err(|e| Error::Other(format!("{:?}", e)))?;
+                    let field_element = F::from_le_bytes_mod_order(&parsed_biguint.to_bytes_be());
+                    external_inputs.push(field_element);
+                }
+                Some(external_inputs)
+            }
+            None => None,
+        };
 
         Ok(NoNameIVCInputs {
             ivc_input,
@@ -109,12 +124,20 @@ impl<F: PrimeField> TryInto<(JsonInputs, JsonInputs)> for NoNameIVCInputs<F> {
     fn try_into(self) -> Result<(JsonInputs, JsonInputs), Error> {
         let ivc_inputs = serde_json::to_string(&NoNameJSONIVCInput::from(self.ivc_input))
             .map_err(|e| Error::Other(format!("{:?}", e)))?;
-        let external_inputs =
-            serde_json::to_string(&NoNameJSONIVCInput::from(self.external_inputs))
+        let external_inputs = match self.external_inputs {
+            Some(inputs) => {
+                let parsed_inputs = parse_inputs(
+                    &serde_json::to_string(&NoNameJSONExternalInputs::from(inputs))
+                        .map_err(|e| Error::Other(format!("{:?}", e)))?,
+                )
                 .map_err(|e| Error::Other(format!("{:?}", e)))?;
+                parsed_inputs
+            }
+            None => parse_inputs(&serde_json::to_string("{blank: []}").unwrap()).unwrap(),
+        };
         Ok((
             parse_inputs(&ivc_inputs).map_err(|e| Error::Other(format!("{:?}", e)))?,
-            parse_inputs(&external_inputs).map_err(|e| Error::Other(format!("{:?}", e)))?,
+            external_inputs,
         ))
     }
 }
@@ -176,20 +199,39 @@ impl<F: PrimeField, BF: BackendField> FCircuit<F> for NoNameFCircuit<F, BF> {
         z_i: Vec<ark_r1cs_std::fields::fp::FpVar<F>>,
         external_inputs: Vec<ark_r1cs_std::fields::fp::FpVar<F>>, // inputs that are not part of the state
     ) -> Result<Vec<ark_r1cs_std::fields::fp::FpVar<F>>, ark_relations::r1cs::SynthesisError> {
-        // build json input values from the z_i vec vector and the external inputs
-        // we will also need to slice the witneess vector but for this, we need to be able to
-        // output public values as arrays
-        let external_inputs =
-            serde_json::to_string(&NoNameJSONExternalInputs::from_fpvars(&external_inputs))
+        // fpvars -> String -> formatted json -> JsonInputs
+        let external_inputs_present = external_inputs.len() != 0;
+        let external_inputs_json = match external_inputs_present {
+            false => None,
+            true => {
+                let json = serde_json::to_string(&NoNameJSONExternalInputs::from_fpvars(
+                    &external_inputs.clone(),
+                ))
                 .unwrap();
+                Some(json)
+            }
+        };
         let ivc_inputs = serde_json::to_string(&NoNameJSONIVCInput::from_fpvars(&z_i)).unwrap();
         let noname_inputs =
-            NoNameIVCInputs::try_from((ivc_inputs.as_str(), external_inputs.as_str())).unwrap();
+            NoNameIVCInputs::<F>::try_from((ivc_inputs, external_inputs_json)).unwrap();
         let (ivc_inputs, external_inputs) = noname_inputs.try_into().unwrap();
         let noname_witness = self
             .circuit
             .generate_witness(ivc_inputs, external_inputs)
             .unwrap();
+
+        let noname_circuit = NoNameCircuit {
+            compiled_circuit: self.circuit.clone(),
+            witness: noname_witness,
+        };
+        noname_circuit.generate_constraints(cs.clone())?;
+
+        assert!(cs.is_satisfied()?);
+        // let z_i1: Vec<FpVar<F>> = Vec::<FpVar<F>>::new_witness(cs.clone(), || {
+        //     Ok(witness[1..1 + self.state_len()].to_vec())
+        // })?;
+
+        Ok(vec![FpVar::new_witness(cs.clone(), || Ok(F::one()))?])
     }
 }
 
@@ -197,87 +239,90 @@ impl<F: PrimeField, BF: BackendField> FCircuit<F> for NoNameFCircuit<F, BF> {
 mod tests {
 
     use ark_bn254::Fr;
+    use ark_noname::circuits::WITH_PUBLIC_OUTPUT_ARRAY;
     use ark_r1cs_std::{alloc::AllocVar, fields::fp::FpVar};
-    use noname::{
-        backends::r1cs::R1csBn254Field,
-        inputs::{parse_inputs, JsonInputs},
-    };
+    use noname::{backends::r1cs::R1csBn254Field, inputs::parse_inputs};
 
     use crate::frontend::FCircuit;
 
     use super::{NoNameExternalInputs, NoNameFCircuit, NoNameIVCInputs};
-    use ark_relations::r1cs::ConstraintSystem;
+    use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystem};
 
     const CIRCUIT_EXTERNAL_INPUTS: &str =
-        "fn main(pub ivc_input: [Field; 2], external_inputs: [Field; 2]) -> [Field; 2]{
+        "fn main(pub ivc_input: [Field; 2], external_inputs: [Field; 2]) -> [Field; 2] {
     let xx = external_inputs[0] + ivc_input[0];
     let yy = external_inputs[1] * ivc_input[1];
     assert_eq(yy, xx);
     return [xx, yy];
 }";
 
+    const CIRCUIT_NO_EXTERNAL_INPUTS: &str = "fn main(pub ivc_input: [Field; 2]) -> [Field; 2] {
+    let out = ivc_input[0] * ivc_input[1];
+    return [out, ivc_input[1]];
+}";
+
     #[test]
     fn test_noname_step_native() {}
 
     #[test]
-    fn test_noname_step_constraints() {
+    fn test_step_constraints() {
         let cs = ConstraintSystem::<Fr>::new_ref();
         let params = (CIRCUIT_EXTERNAL_INPUTS.to_owned(), 2, 2);
         let circuit = NoNameFCircuit::<Fr, R1csBn254Field>::new(params).unwrap();
         let inputs_public = r#"{"ivc_input":["2","5"]}"#;
         let inputs_private = r#"{"external_inputs":["8","2"]}"#;
-        let ivc_inputs = NoNameIVCInputs::try_from((inputs_public, inputs_private)).unwrap();
+        let ivc_inputs =
+            NoNameIVCInputs::try_from((inputs_public.to_owned(), Some(inputs_private.to_owned())))
+                .unwrap();
 
         let ivc_inputs_var =
             Vec::<FpVar<Fr>>::new_witness(cs.clone(), || Ok(ivc_inputs.ivc_input)).unwrap();
+        let external_inputs_var =
+            Vec::<FpVar<Fr>>::new_witness(cs.clone(), || Ok(ivc_inputs.external_inputs.unwrap()))
+                .unwrap();
 
-        let json_public = parse_inputs(inputs_public).unwrap();
-        let json_private = parse_inputs(inputs_private).unwrap();
-        //let witness = circuit
-        //    .circuit
-        //    .generate_witness(json_public, json_private)
-        //    .unwrap();
-        // convert json inputs to fp vars
+        circuit.generate_step_constraints(cs.clone(), 0, ivc_inputs_var, external_inputs_var);
     }
 
-    //fn assign_json_inputs_as_fpvars<F: PrimeField>(
-    //    cs: ConstraintSystemRef<F>,
-    //    json_input: &JsonInputs,
-    //) -> Result<(Vec<FpVar<F>>, Vec<FpVar<F>>), Error> {
-    //    let ivc_inputs = match json_input.0.get("ivc_input") {
-    //        Some(ivc_input_array) => {
-    //            let fp_vars_public_inputs = match ivc_input_array {
-    //                serde_json::Value::Array(ivc_values) => {
-    //                    let ivc_field_elements: Vec<F> = Vec::with_capacity(ivc_values.len());
-    //                    for value in ivc_values {
-    //                        match value {
-    //                            serde_json::Value::Number(element)=> {
-    //                                let element = element.n;
-    //                            },
-    //                            _ => return Err(Error::Other("Can not accept a type different from number for IVC inputs".to_owned()))
-    //                        }
-    //                    }
+    #[test]
+    fn test_wrapper_to_nonamecircuit() {
+        let params = (CIRCUIT_NO_EXTERNAL_INPUTS.to_string(), 2, 2);
+        let noname_fcircuit = NoNameFCircuit::<Fr, R1csBn254Field>::new(params).unwrap();
 
-    //                }
-    //                _ => return Err(Error::Other("IVC inputs should be an array".to_owned())),
-    //            };
-    //        }
-    //        None => {
-    //            return Err(Error::Other(
-    //                "There should at least be public IVC inputs set!".to_owned(),
-    //            ))
-    //        }
-    //    };
+        // Allocates z_i1 by using step_native function.
+        let z_i = vec![Fr::from(2_u32), Fr::from(5_u32)];
+        //let wrapper_circuit = crate::frontend::tests::WrapperCircuit {
+        //    FC: noname_fcircuit.clone(),
+        //    z_i: Some(z_i.clone()),
+        //    z_i1: Some(noname_fcircuit.step_native(0, z_i.clone(), vec![]).unwrap()),
+        //};
 
-    //    let external_inputs = match json_input.0.get("external_inputs") {
-    //        Some(external_inputs) => {}
-    //        None => Ok(),
-    //    }
-    //}
+        //let cs = ConstraintSystem::<Fr>::new_ref();
+
+        //wrapper_circuit.generate_constraints(cs.clone()).unwrap();
+        //assert!(
+        //    cs.is_satisfied().unwrap(),
+        //    "Constraint system is not satisfied"
+        //);
+    }
 
     #[test]
     fn test_external_inputs() {}
 
     #[test]
-    fn test_no_external_inputs() {}
+    fn test_no_external_inputs() {
+        let cs = ConstraintSystem::<Fr>::new_ref();
+        let params = (CIRCUIT_NO_EXTERNAL_INPUTS.to_owned(), 2, 2);
+        let circuit = NoNameFCircuit::<Fr, R1csBn254Field>::new(params).unwrap();
+        let inputs_public = r#"{"ivc_input":["2","5"]}"#;
+        let ivc_inputs = NoNameIVCInputs::try_from((inputs_public.to_owned(), None)).unwrap();
+
+        let ivc_inputs_var =
+            Vec::<FpVar<Fr>>::new_witness(cs.clone(), || Ok(ivc_inputs.ivc_input)).unwrap();
+
+        let json_public = parse_inputs(inputs_public).unwrap();
+
+        let params = (WITH_PUBLIC_OUTPUT_ARRAY.to_string(), 2, 2);
+        let f_circuit = NoNameFCircuit::<Fr, R1csBn254Field>::new(params).unwrap();
+    }
 }
