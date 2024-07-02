privacy-scaling-explorations/sonobe

Impl `Transcript{Var}` for `PoseidonSponge{Var}` directly and remove `PoseidonTranscript{Var}`

folding-schemes/src/commitment/ipa.rs
@@ -90,7 +90,7 @@ impl<C: CurveGroup, const H: bool> CommitmentScheme<C, H> for IPA<C, H> {
 
     fn prove(
         params: &Self::ProverParams,
-        transcript: &mut impl Transcript<C>,
+        transcript: &mut impl Transcript<C::ScalarField>,
         P: &C,                // commitment
         a: &[C::ScalarField], // vector
         blind: &C::ScalarField,
@@ -218,7 +218,7 @@ impl<C: CurveGroup, const H: bool> CommitmentScheme<C, H> for IPA<C, H> {
 
     fn verify(
         params: &Self::VerifierParams,
-        transcript: &mut impl Transcript<C>,
+        transcript: &mut impl Transcript<C::ScalarField>,
         P: &C, // commitment
         proof: &Self::Proof,
     ) -> Result<(), Error> {
@@ -559,15 +559,15 @@ where
 
 #[cfg(test)]
 mod tests {
+    use ark_crypto_primitives::sponge::{poseidon::PoseidonSponge, CryptographicSponge};
     use ark_ec::Group;
     use ark_pallas::{constraints::GVar, Fq, Fr, Projective};
-    use ark_r1cs_std::{alloc::AllocVar, bits::boolean::Boolean, eq::EqGadget};
+    use ark_r1cs_std::eq::EqGadget;
     use ark_relations::r1cs::ConstraintSystem;
-    use ark_std::UniformRand;
     use std::ops::Mul;
 
     use super::*;
-    use crate::transcript::poseidon::{poseidon_test_config, PoseidonTranscript};
+    use crate::transcript::poseidon::poseidon_test_config;
 
     #[test]
     fn test_ipa() {
@@ -585,9 +585,9 @@ mod tests {
 
         let poseidon_config = poseidon_test_config::<Fr>();
         // init Prover's transcript
-        let mut transcript_p = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
         // init Verifier's transcript
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
 
         // a is the vector that we're committing
         let a: Vec<Fr> = std::iter::repeat_with(|| Fr::rand(&mut rng))
@@ -629,9 +629,9 @@ mod tests {
 
         let poseidon_config = poseidon_test_config::<Fr>();
         // init Prover's transcript
-        let mut transcript_p = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
         // init Verifier's transcript
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
 
         let mut a: Vec<Fr> = std::iter::repeat_with(|| Fr::rand(&mut rng))
             .take(d / 2)
@@ -659,7 +659,7 @@ mod tests {
         // circuit
         let cs = ConstraintSystem::<Fq>::new_ref();
 
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
         transcript_v.absorb_point(&cm).unwrap();
         let challenge = transcript_v.get_challenge(); // challenge value at which we evaluate
         let s = transcript_v.get_challenge();

folding-schemes/src/commitment/mod.rs
@@ -31,7 +31,7 @@ pub trait CommitmentScheme<C: CurveGroup, const H: bool = false>: Clone + Debug 
 
     fn prove(
         params: &Self::ProverParams,
-        transcript: &mut impl Transcript<C>,
+        transcript: &mut impl Transcript<C::ScalarField>,
         cm: &C,
         v: &[C::ScalarField],
         blind: &C::ScalarField,
@@ -50,7 +50,7 @@ pub trait CommitmentScheme<C: CurveGroup, const H: bool = false>: Clone + Debug 
 
     fn verify(
         params: &Self::VerifierParams,
-        transcript: &mut impl Transcript<C>,
+        transcript: &mut impl Transcript<C::ScalarField>,
         cm: &C,
         proof: &Self::Proof,
     ) -> Result<(), Error>;
@@ -69,7 +69,10 @@ pub trait CommitmentScheme<C: CurveGroup, const H: bool = false>: Clone + Debug 
 mod tests {
     use super::*;
     use ark_bn254::{Bn254, Fr, G1Projective as G1};
-    use ark_crypto_primitives::sponge::{poseidon::PoseidonConfig, Absorb};
+    use ark_crypto_primitives::sponge::{
+        poseidon::{PoseidonConfig, PoseidonSponge},
+        Absorb, CryptographicSponge,
+    };
     use ark_poly_commit::kzg10::VerifierKey;
     use ark_std::Zero;
     use ark_std::{test_rng, UniformRand};
@@ -77,10 +80,7 @@ mod tests {
     use super::ipa::IPA;
     use super::kzg::{ProverKey, KZG};
     use super::pedersen::Pedersen;
-    use crate::transcript::{
-        poseidon::{poseidon_test_config, PoseidonTranscript},
-        Transcript,
-    };
+    use crate::transcript::poseidon::poseidon_test_config;
 
     #[test]
     fn test_homomorphic_property_using_Commitment_trait() {
@@ -150,7 +150,7 @@ mod tests {
         let v_3: Vec<C::ScalarField> = v_1.iter().zip(v_2).map(|(a, b)| *a + (r * b)).collect();
 
         // compute the proof of the cm_3
-        let transcript_p = &mut PoseidonTranscript::<C>::new(poseidon_config);
+        let transcript_p = &mut PoseidonSponge::<C::ScalarField>::new(poseidon_config);
         let proof = CS::prove(
             prover_params,
             transcript_p,
@@ -162,7 +162,7 @@ mod tests {
         .unwrap();
 
         // verify the opening proof
-        let transcript_v = &mut PoseidonTranscript::<C>::new(poseidon_config);
+        let transcript_v = &mut PoseidonSponge::<C::ScalarField>::new(poseidon_config);
         CS::verify(verifier_params, transcript_v, &cm_3, &proof).unwrap();
     }
 }

folding-schemes/src/folding/nova/nifs.rs
@@ -183,7 +183,7 @@ where
     }
 
     pub fn prove_commitments(
-        tr: &mut impl Transcript<C>,
+        tr: &mut impl Transcript<C::ScalarField>,
         cs_prover_params: &CS::ProverParams,
         w: &Witness<C>,
         ci: &CommittedInstance<C>,
@@ -200,7 +200,10 @@ where
 #[cfg(test)]
 pub mod tests {
     use super::*;
-    use ark_crypto_primitives::sponge::poseidon::PoseidonConfig;
+    use ark_crypto_primitives::sponge::{
+        poseidon::{PoseidonConfig, PoseidonSponge},
+        CryptographicSponge,
+    };
     use ark_ff::{BigInteger, PrimeField};
     use ark_pallas::{Fr, Projective};
     use ark_std::{ops::Mul, UniformRand};
@@ -209,7 +212,7 @@ pub mod tests {
     use crate::commitment::pedersen::{Params as PedersenParams, Pedersen};
     use crate::folding::nova::circuits::ChallengeGadget;
     use crate::folding::nova::traits::NovaR1CS;
-    use crate::transcript::poseidon::{poseidon_test_config, PoseidonTranscript};
+    use crate::transcript::poseidon::poseidon_test_config;
 
     #[allow(clippy::type_complexity)]
     pub(crate) fn prepare_simple_fold_inputs<C>() -> (
@@ -362,9 +365,9 @@ pub mod tests {
             .unwrap();
 
         // init Prover's transcript
-        let mut transcript_p = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
         // init Verifier's transcript
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
 
         // prove the ci3.cmE, ci3.cmW, cmT commitments
         let cm_proofs = NIFS::<Projective, Pedersen<Projective>>::prove_commitments(

folding-schemes/src/folding/protogalaxy/folding.rs
@@ -6,12 +6,10 @@ use ark_poly::{
     univariate::{DensePolynomial, SparsePolynomial},
     DenseUVPolynomial, EvaluationDomain, Evaluations, GeneralEvaluationDomain, Polynomial,
 };
-use ark_std::log2;
-use ark_std::{cfg_into_iter, Zero};
+use ark_std::{cfg_into_iter, log2, Zero};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::marker::PhantomData;
 
-use super::traits::ProtoGalaxyTranscript;
 use super::utils::{all_powers, betas_star, exponential_powers};
 use super::ProtoGalaxyError;
 use super::{CommittedInstance, Witness};
@@ -36,7 +34,7 @@ where
     #![allow(clippy::type_complexity)]
     /// implements the non-interactive Prover from the folding scheme described in section 4
     pub fn prove(
-        transcript: &mut (impl Transcript<C> + ProtoGalaxyTranscript<C>),
+        transcript: &mut impl Transcript<C::ScalarField>,
         r1cs: &R1CS<C::ScalarField>,
         // running instance
         instance: &CommittedInstance<C>,
@@ -81,8 +79,8 @@ where
         }
 
         // absorb the committed instances
-        transcript.absorb_committed_instance(instance)?;
-        transcript.absorb_committed_instances(&vec_instances)?;
+        transcript.absorb(instance);
+        transcript.absorb(&vec_instances);
 
         let delta = transcript.get_challenge();
         let deltas = exponential_powers(delta, t);
@@ -93,7 +91,7 @@ where
         let F_X: SparsePolynomial<C::ScalarField> =
             calc_f_from_btree(&f_w, &instance.betas, &deltas).expect("Error calculating F[x]");
         let F_X_dense = DensePolynomial::from(F_X.clone());
-        transcript.absorb_vec(&F_X_dense.coeffs);
+        transcript.absorb(&F_X_dense.coeffs);
 
         let alpha = transcript.get_challenge();
 
@@ -185,7 +183,7 @@ where
             return Err(Error::ProtoGalaxy(ProtoGalaxyError::RemainderNotZero));
         }
 
-        transcript.absorb_vec(&K_X.coeffs);
+        transcript.absorb(&K_X.coeffs);
 
         let gamma = transcript.get_challenge();
 
@@ -221,7 +219,7 @@ where
 
     /// implements the non-interactive Verifier from the folding scheme described in section 4
     pub fn verify(
-        transcript: &mut (impl Transcript<C> + ProtoGalaxyTranscript<C>),
+        transcript: &mut impl Transcript<C::ScalarField>,
         r1cs: &R1CS<C::ScalarField>,
         // running instance
         instance: &CommittedInstance<C>,
@@ -235,13 +233,13 @@ where
         let n = r1cs.A.n_cols;
 
         // absorb the committed instances
-        transcript.absorb_committed_instance(instance)?;
-        transcript.absorb_committed_instances(&vec_instances)?;
+        transcript.absorb(instance);
+        transcript.absorb(&vec_instances);
 
         let delta = transcript.get_challenge();
         let deltas = exponential_powers(delta, t);
 
-        transcript.absorb_vec(&F_coeffs);
+        transcript.absorb(&F_coeffs);
 
         let alpha = transcript.get_challenge();
         let alphas = all_powers(alpha, n);
@@ -262,7 +260,7 @@ where
         let K_X: DensePolynomial<C::ScalarField> =
             DensePolynomial::<C::ScalarField>::from_coefficients_vec(K_coeffs);
 
-        transcript.absorb_vec(&K_X.coeffs);
+        transcript.absorb(&K_X.coeffs);
 
         let gamma = transcript.get_challenge();
 
@@ -376,12 +374,14 @@ fn eval_f<F: PrimeField>(r1cs: &R1CS<F>, w: &[F]) -> Result<Vec<F>, Error> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ark_crypto_primitives::sponge::poseidon::PoseidonSponge;
+    use ark_crypto_primitives::sponge::CryptographicSponge;
     use ark_pallas::{Fr, Projective};
     use ark_std::UniformRand;
 
     use crate::ccs::r1cs::tests::{get_test_r1cs, get_test_z};
     use crate::commitment::{pedersen::Pedersen, CommitmentScheme};
-    use crate::transcript::poseidon::{poseidon_test_config, PoseidonTranscript};
+    use crate::transcript::poseidon::poseidon_test_config;
 
     pub(crate) fn check_instance<C: CurveGroup>(
         r1cs: &R1CS<C::ScalarField>,
@@ -509,8 +509,8 @@ mod tests {
 
         // init Prover & Verifier's transcript
         let poseidon_config = poseidon_test_config::<Fr>();
-        let mut transcript_p = PoseidonTranscript::<Projective>::new(&poseidon_config);
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
 
         let (folded_instance, folded_witness, F_coeffs, K_coeffs) = Folding::<Projective>::prove(
             &mut transcript_p,
@@ -549,8 +549,8 @@ mod tests {
 
         // init Prover & Verifier's transcript
         let poseidon_config = poseidon_test_config::<Fr>();
-        let mut transcript_p = PoseidonTranscript::<Projective>::new(&poseidon_config);
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
 
         let (mut running_witness, mut running_instance, _, _) = prepare_inputs(0);
 

solidity-verifiers/src/verifiers/kzg.rs
@@ -78,7 +78,8 @@ mod tests {
         utils::HeaderInclusion,
         ProtocolVerifierKey,
     };
-    use ark_bn254::{Bn254, Fr, G1Projective as G1};
+    use ark_bn254::{Bn254, Fr};
+    use ark_crypto_primitives::sponge::{poseidon::PoseidonSponge, CryptographicSponge};
     use ark_ec::{AffineRepr, CurveGroup};
     use ark_ff::{BigInteger, PrimeField};
     use ark_std::rand::{RngCore, SeedableRng};
@@ -89,10 +90,7 @@ mod tests {
 
     use folding_schemes::{
         commitment::{kzg::KZG, CommitmentScheme},
-        transcript::{
-            poseidon::{poseidon_test_config, PoseidonTranscript},
-            Transcript,
-        },
+        transcript::{poseidon::poseidon_test_config, Transcript},
     };
 
     use super::KZG10Verifier;
@@ -133,8 +131,8 @@ mod tests {
     fn kzg_verifier_accepts_and_rejects_proofs() {
         let mut rng = ark_std::rand::rngs::StdRng::seed_from_u64(test_rng().next_u64());
         let poseidon_config = poseidon_test_config::<Fr>();
-        let transcript_p = &mut PoseidonTranscript::<G1>::new(&poseidon_config);
-        let transcript_v = &mut PoseidonTranscript::<G1>::new(&poseidon_config);
+        let transcript_p = &mut PoseidonSponge::<Fr>::new(&poseidon_config);
+        let transcript_v = &mut PoseidonSponge::<Fr>::new(&poseidon_config);
 
         let (kzg_pk, kzg_vk, _, _, _) = setup(DEFAULT_SETUP_LEN);
         let kzg_vk = KZG10VerifierKey::from((kzg_vk.clone(), kzg_pk.powers_of_g[0..3].to_vec()));

privacy-scaling-explorations/folding-schemes

folding-schemes/src/commitment/ipa.rs
@@ -90,7 +90,7 @@ impl<C: CurveGroup, const H: bool> CommitmentScheme<C, H> for IPA<C, H> {
 
     fn prove(
         params: &Self::ProverParams,
-        transcript: &mut impl Transcript<C>,
+        transcript: &mut impl Transcript<C::ScalarField>,
         P: &C,                // commitment
         a: &[C::ScalarField], // vector
         blind: &C::ScalarField,
@@ -218,7 +218,7 @@ impl<C: CurveGroup, const H: bool> CommitmentScheme<C, H> for IPA<C, H> {
 
     fn verify(
         params: &Self::VerifierParams,
-        transcript: &mut impl Transcript<C>,
+        transcript: &mut impl Transcript<C::ScalarField>,
         P: &C, // commitment
         proof: &Self::Proof,
     ) -> Result<(), Error> {
@@ -559,15 +559,15 @@ where
 
 #[cfg(test)]
 mod tests {
+    use ark_crypto_primitives::sponge::{poseidon::PoseidonSponge, CryptographicSponge};
     use ark_ec::Group;
     use ark_pallas::{constraints::GVar, Fq, Fr, Projective};
-    use ark_r1cs_std::{alloc::AllocVar, bits::boolean::Boolean, eq::EqGadget};
+    use ark_r1cs_std::eq::EqGadget;
     use ark_relations::r1cs::ConstraintSystem;
-    use ark_std::UniformRand;
     use std::ops::Mul;
 
     use super::*;
-    use crate::transcript::poseidon::{poseidon_canonical_config, PoseidonTranscript};
+    use crate::transcript::poseidon::poseidon_canonical_config;
 
     #[test]
     fn test_ipa() {
@@ -585,9 +585,9 @@ mod tests {
 
         let poseidon_config = poseidon_canonical_config::<Fr>();
         // init Prover's transcript
-        let mut transcript_p = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
         // init Verifier's transcript
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
 
         // a is the vector that we're committing
         let a: Vec<Fr> = std::iter::repeat_with(|| Fr::rand(&mut rng))
@@ -629,9 +629,9 @@ mod tests {
 
         let poseidon_config = poseidon_canonical_config::<Fr>();
         // init Prover's transcript
-        let mut transcript_p = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
         // init Verifier's transcript
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
 
         let mut a: Vec<Fr> = std::iter::repeat_with(|| Fr::rand(&mut rng))
             .take(d / 2)
@@ -659,7 +659,7 @@ mod tests {
         // circuit
         let cs = ConstraintSystem::<Fq>::new_ref();
 
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
         transcript_v.absorb_point(&cm).unwrap();
         let challenge = transcript_v.get_challenge(); // challenge value at which we evaluate
         let s = transcript_v.get_challenge();

folding-schemes/src/commitment/mod.rs
@@ -31,7 +31,7 @@ pub trait CommitmentScheme<C: CurveGroup, const H: bool = false>: Clone + Debug 
 
     fn prove(
         params: &Self::ProverParams,
-        transcript: &mut impl Transcript<C>,
+        transcript: &mut impl Transcript<C::ScalarField>,
         cm: &C,
         v: &[C::ScalarField],
         blind: &C::ScalarField,
@@ -50,7 +50,7 @@ pub trait CommitmentScheme<C: CurveGroup, const H: bool = false>: Clone + Debug 
 
     fn verify(
         params: &Self::VerifierParams,
-        transcript: &mut impl Transcript<C>,
+        transcript: &mut impl Transcript<C::ScalarField>,
         cm: &C,
         proof: &Self::Proof,
     ) -> Result<(), Error>;
@@ -69,7 +69,10 @@ pub trait CommitmentScheme<C: CurveGroup, const H: bool = false>: Clone + Debug 
 mod tests {
     use super::*;
     use ark_bn254::{Bn254, Fr, G1Projective as G1};
-    use ark_crypto_primitives::sponge::{poseidon::PoseidonConfig, Absorb};
+    use ark_crypto_primitives::sponge::{
+        poseidon::{PoseidonConfig, PoseidonSponge},
+        Absorb, CryptographicSponge,
+    };
     use ark_poly_commit::kzg10::VerifierKey;
     use ark_std::Zero;
     use ark_std::{test_rng, UniformRand};
@@ -77,10 +80,7 @@ mod tests {
     use super::ipa::IPA;
     use super::kzg::{ProverKey, KZG};
     use super::pedersen::Pedersen;
-    use crate::transcript::{
-        poseidon::{poseidon_canonical_config, PoseidonTranscript},
-        Transcript,
-    };
+    use crate::transcript::poseidon::poseidon_canonical_config;
 
     #[test]
     fn test_homomorphic_property_using_Commitment_trait() {
@@ -150,7 +150,7 @@ mod tests {
         let v_3: Vec<C::ScalarField> = v_1.iter().zip(v_2).map(|(a, b)| *a + (r * b)).collect();
 
         // compute the proof of the cm_3
-        let transcript_p = &mut PoseidonTranscript::<C>::new(poseidon_config);
+        let transcript_p = &mut PoseidonSponge::<C::ScalarField>::new(poseidon_config);
         let proof = CS::prove(
             prover_params,
             transcript_p,
@@ -162,7 +162,7 @@ mod tests {
         .unwrap();
 
         // verify the opening proof
-        let transcript_v = &mut PoseidonTranscript::<C>::new(poseidon_config);
+        let transcript_v = &mut PoseidonSponge::<C::ScalarField>::new(poseidon_config);
         CS::verify(verifier_params, transcript_v, &cm_3, &proof).unwrap();
     }
 }

folding-schemes/src/folding/nova/nifs.rs
@@ -183,7 +183,7 @@ where
     }
 
     pub fn prove_commitments(
-        tr: &mut impl Transcript<C>,
+        tr: &mut impl Transcript<C::ScalarField>,
         cs_prover_params: &CS::ProverParams,
         w: &Witness<C>,
         ci: &CommittedInstance<C>,
@@ -200,7 +200,10 @@ where
 #[cfg(test)]
 pub mod tests {
     use super::*;
-    use ark_crypto_primitives::sponge::poseidon::PoseidonConfig;
+    use ark_crypto_primitives::sponge::{
+        poseidon::{PoseidonConfig, PoseidonSponge},
+        CryptographicSponge,
+    };
     use ark_ff::{BigInteger, PrimeField};
     use ark_pallas::{Fr, Projective};
     use ark_std::{ops::Mul, UniformRand};
@@ -209,7 +212,7 @@ pub mod tests {
     use crate::commitment::pedersen::{Params as PedersenParams, Pedersen};
     use crate::folding::nova::circuits::ChallengeGadget;
     use crate::folding::nova::traits::NovaR1CS;
-    use crate::transcript::poseidon::{poseidon_canonical_config, PoseidonTranscript};
+    use crate::transcript::poseidon::poseidon_canonical_config;
 
     #[allow(clippy::type_complexity)]
     pub(crate) fn prepare_simple_fold_inputs<C>() -> (
@@ -362,9 +365,9 @@ pub mod tests {
             .unwrap();
 
         // init Prover's transcript
-        let mut transcript_p = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
         // init Verifier's transcript
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
 
         // prove the ci3.cmE, ci3.cmW, cmT commitments
         let cm_proofs = NIFS::<Projective, Pedersen<Projective>>::prove_commitments(

folding-schemes/src/folding/protogalaxy/folding.rs
@@ -6,12 +6,10 @@ use ark_poly::{
     univariate::{DensePolynomial, SparsePolynomial},
     DenseUVPolynomial, EvaluationDomain, Evaluations, GeneralEvaluationDomain, Polynomial,
 };
-use ark_std::log2;
-use ark_std::{cfg_into_iter, Zero};
+use ark_std::{cfg_into_iter, log2, Zero};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::marker::PhantomData;
 
-use super::traits::ProtoGalaxyTranscript;
 use super::utils::{all_powers, betas_star, exponential_powers};
 use super::ProtoGalaxyError;
 use super::{CommittedInstance, Witness};
@@ -36,7 +34,7 @@ where
     #![allow(clippy::type_complexity)]
     /// implements the non-interactive Prover from the folding scheme described in section 4
     pub fn prove(
-        transcript: &mut (impl Transcript<C> + ProtoGalaxyTranscript<C>),
+        transcript: &mut impl Transcript<C::ScalarField>,
         r1cs: &R1CS<C::ScalarField>,
         // running instance
         instance: &CommittedInstance<C>,
@@ -81,8 +79,8 @@ where
         }
 
         // absorb the committed instances
-        transcript.absorb_committed_instance(instance)?;
-        transcript.absorb_committed_instances(&vec_instances)?;
+        transcript.absorb(instance);
+        transcript.absorb(&vec_instances);
 
         let delta = transcript.get_challenge();
         let deltas = exponential_powers(delta, t);
@@ -93,7 +91,7 @@ where
         let F_X: SparsePolynomial<C::ScalarField> =
             calc_f_from_btree(&f_w, &instance.betas, &deltas).expect("Error calculating F[x]");
         let F_X_dense = DensePolynomial::from(F_X.clone());
-        transcript.absorb_vec(&F_X_dense.coeffs);
+        transcript.absorb(&F_X_dense.coeffs);
 
         let alpha = transcript.get_challenge();
 
@@ -185,7 +183,7 @@ where
             return Err(Error::ProtoGalaxy(ProtoGalaxyError::RemainderNotZero));
         }
 
-        transcript.absorb_vec(&K_X.coeffs);
+        transcript.absorb(&K_X.coeffs);
 
         let gamma = transcript.get_challenge();
 
@@ -221,7 +219,7 @@ where
 
     /// implements the non-interactive Verifier from the folding scheme described in section 4
     pub fn verify(
-        transcript: &mut (impl Transcript<C> + ProtoGalaxyTranscript<C>),
+        transcript: &mut impl Transcript<C::ScalarField>,
         r1cs: &R1CS<C::ScalarField>,
         // running instance
         instance: &CommittedInstance<C>,
@@ -235,13 +233,13 @@ where
         let n = r1cs.A.n_cols;
 
         // absorb the committed instances
-        transcript.absorb_committed_instance(instance)?;
-        transcript.absorb_committed_instances(&vec_instances)?;
+        transcript.absorb(instance);
+        transcript.absorb(&vec_instances);
 
         let delta = transcript.get_challenge();
         let deltas = exponential_powers(delta, t);
 
-        transcript.absorb_vec(&F_coeffs);
+        transcript.absorb(&F_coeffs);
 
         let alpha = transcript.get_challenge();
         let alphas = all_powers(alpha, n);
@@ -262,7 +260,7 @@ where
         let K_X: DensePolynomial<C::ScalarField> =
             DensePolynomial::<C::ScalarField>::from_coefficients_vec(K_coeffs);
 
-        transcript.absorb_vec(&K_X.coeffs);
+        transcript.absorb(&K_X.coeffs);
 
         let gamma = transcript.get_challenge();
 
@@ -376,12 +374,14 @@ fn eval_f<F: PrimeField>(r1cs: &R1CS<F>, w: &[F]) -> Result<Vec<F>, Error> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ark_crypto_primitives::sponge::poseidon::PoseidonSponge;
+    use ark_crypto_primitives::sponge::CryptographicSponge;
     use ark_pallas::{Fr, Projective};
     use ark_std::UniformRand;
 
     use crate::ccs::r1cs::tests::{get_test_r1cs, get_test_z};
     use crate::commitment::{pedersen::Pedersen, CommitmentScheme};
-    use crate::transcript::poseidon::{poseidon_canonical_config, PoseidonTranscript};
+    use crate::transcript::poseidon::poseidon_canonical_config;
 
     pub(crate) fn check_instance<C: CurveGroup>(
         r1cs: &R1CS<C::ScalarField>,
@@ -509,8 +509,8 @@ mod tests {
 
         // init Prover & Verifier's transcript
         let poseidon_config = poseidon_canonical_config::<Fr>();
-        let mut transcript_p = PoseidonTranscript::<Projective>::new(&poseidon_config);
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
 
         let (folded_instance, folded_witness, F_coeffs, K_coeffs) = Folding::<Projective>::prove(
             &mut transcript_p,
@@ -549,8 +549,8 @@ mod tests {
 
         // init Prover & Verifier's transcript
         let poseidon_config = poseidon_canonical_config::<Fr>();
-        let mut transcript_p = PoseidonTranscript::<Projective>::new(&poseidon_config);
-        let mut transcript_v = PoseidonTranscript::<Projective>::new(&poseidon_config);
+        let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
+        let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
 
         let (mut running_witness, mut running_instance, _, _) = prepare_inputs(0);
 

solidity-verifiers/src/verifiers/kzg.rs
@@ -78,7 +78,8 @@ mod tests {
         utils::HeaderInclusion,
         ProtocolVerifierKey,
     };
-    use ark_bn254::{Bn254, Fr, G1Projective as G1};
+    use ark_bn254::{Bn254, Fr};
+    use ark_crypto_primitives::sponge::{poseidon::PoseidonSponge, CryptographicSponge};
     use ark_ec::{AffineRepr, CurveGroup};
     use ark_ff::{BigInteger, PrimeField};
     use ark_std::rand::{RngCore, SeedableRng};
@@ -89,10 +90,7 @@ mod tests {
 
     use folding_schemes::{
         commitment::{kzg::KZG, CommitmentScheme},
-        transcript::{
-            poseidon::{poseidon_canonical_config, PoseidonTranscript},
-            Transcript,
-        },
+        transcript::{poseidon::poseidon_canonical_config, Transcript},
     };
 
     use super::KZG10Verifier;
@@ -133,8 +131,8 @@ mod tests {
     fn kzg_verifier_accepts_and_rejects_proofs() {
         let mut rng = ark_std::rand::rngs::StdRng::seed_from_u64(test_rng().next_u64());
         let poseidon_config = poseidon_canonical_config::<Fr>();
-        let transcript_p = &mut PoseidonTranscript::<G1>::new(&poseidon_config);
-        let transcript_v = &mut PoseidonTranscript::<G1>::new(&poseidon_config);
+        let transcript_p = &mut PoseidonSponge::<Fr>::new(&poseidon_config);
+        let transcript_v = &mut PoseidonSponge::<Fr>::new(&poseidon_config);
 
         let (kzg_pk, kzg_vk, _, _, _) = setup(DEFAULT_SETUP_LEN);
         let kzg_vk = KZG10VerifierKey::from((kzg_vk.clone(), kzg_pk.powers_of_g[0..3].to_vec()));